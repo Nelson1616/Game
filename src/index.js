5ee38c741ba7029b13ddacc467e9057952ab594a
Nelson Neto <nelson16.code@gmail.com>
2023-11-01 02:58:59 -0400

    feat: add sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import logger from 'morgan';
+import sass from 'node-sass-middleware';
 
 import apiRouter from './routes/api.js';
 import appRouter from './routes/app.js';
@@ -10,10 +11,20 @@ const port = 8080;
 const app = express();
 
 app.use(express.json());
+
 app.use(logger('short'));
+
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+
+app.use(sass({
+    src: './public/scss',
+    dest: './public/css',
+    outputStyle: 'compressed',
+    prefix: '/css',
+}));
 
 app.use('/images', [
     express.static('./public/images')
