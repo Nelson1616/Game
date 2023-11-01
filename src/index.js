44fbcae40e5dcc9c6a090a8acd0e4865dbc35a35
Nelson Neto <nelson16.code@gmail.com>
2023-11-01 01:17:46 -0400

    feat: add mogan logger middlewere

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import logger from 'morgan';
+
 import apiRouter from './routes/api.js';
 import appRouter from './routes/app.js';
 
@@ -8,6 +10,7 @@ const port = 8080;
 const app = express();
 
 app.use(express.json());
+app.use(logger('short'));
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
